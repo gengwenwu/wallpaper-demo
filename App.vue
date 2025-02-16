1fec40a8321dec2c844e35742b6ee835d21bef50
Logan <490211581@qq.com>
2025-02-16 12:24:25 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* abc  */
+	/* 导入自定义样式 */
+	@import "common/style/common-style.scss"
+	
+</style>
---
+++ README.md
@@ -0,0 +1,2 @@
+# wallpaper-demo
+ uniapp vue3 壁纸demo练习项目
